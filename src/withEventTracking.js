69ff7314707a3aaae6b2b7608294d7f538a08993
Misha Moroshko <michael.moroshko@gmail.com>
2019-10-11 21:52:00 +1100

    Add noContextInject escape hatch

--- src/withEventTracking.js
+++ src/withEventTracking.js
@@ -2,25 +2,33 @@ import React from "react";
 import { SiteContext } from "./withSiteTracking";
 import { PageContext } from "./withPageTracking";
 
-function withEventTracking(Component) {
+function withEventTracking(Component, noContextInject) {
   return function WithEventTracking(props) {
     return (
       <SiteContext.Consumer>
-        {({ siteData, inject = {} }) => (
-          <PageContext.Consumer>
-            {pageData => (
-              <Component
-                {...props}
-                {...Object.keys(inject).reduce((acc, prop) => {
-                  acc[prop] = eventData => {
-                    return inject[prop]({ siteData, pageData, eventData });
-                  };
-                  return acc;
-                }, {})}
-              />
-            )}
-          </PageContext.Consumer>
-        )}
+        {data => {
+          if (!data) {
+            return <Component {...props} {...noContextInject} />;
+          }
+
+          const { siteData, inject = {} } = data;
+
+          return (
+            <PageContext.Consumer>
+              {pageData => (
+                <Component
+                  {...props}
+                  {...Object.keys(inject).reduce((acc, prop) => {
+                    acc[prop] = eventData => {
+                      return inject[prop]({ siteData, pageData, eventData });
+                    };
+                    return acc;
+                  }, {})}
+                />
+              )}
+            </PageContext.Consumer>
+          );
+        }}
       </SiteContext.Consumer>
     );
   };
