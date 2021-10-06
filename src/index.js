0721e2a3db0e978858d11e5442a2c877844e7f78
Strahinja Ajvaz <19544513+strahinjaajvaz@users.noreply.github.com>
2021-10-06 14:01:23 +1100

    exporting useSiteData (#12)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as useSiteTracking } from "./useSiteTracking";
+export { default as useSiteTracking, useSiteData } from "./useSiteTracking";
 export { default as usePageTracking } from "./usePageTracking";
 export { default as useEventTracking } from "./useEventTracking";
